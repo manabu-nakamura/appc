9f571d52263eec1d9105a3d7a5bab7fa96197232
manabu-nakamura <ma.nakamura@gmail.com>
2026-07-16 15:42:25 +0900

    Update

--- counter/src/main/java/com/github/manabu_nakamura/counter/MainActivity.kt
+++ counter/src/main/java/com/github/manabu_nakamura/counter/MainActivity.kt
@@ -20,18 +20,26 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonGroup
 import androidx.compose.material3.ButtonGroupDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.ToggleButton
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.LinkAnnotation
@@ -51,6 +59,7 @@ import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     @OptIn(
+        ExperimentalMaterial3Api::class,
         ExperimentalMaterial3ExpressiveApi::class
     )
     override fun onCreate(
@@ -259,7 +268,7 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
-/*                        TooltipBox(
+                        TooltipBox(
                             TooltipDefaults.rememberTooltipPositionProvider(
                                 TooltipAnchorPosition.Above
                             ),
@@ -289,18 +298,6 @@ class MainActivity : ComponentActivity() {
                                     )
                                 )
                             }
-                        }*/
-                        TextButton(
-                            {
-                                openDialog = true
-                            },
-                            ButtonDefaults.shapes()
-                        ) {
-                            Text(
-                                stringResource(
-                                    R.string.about
-                                )
-                            )
                         }
                     }
                 }
